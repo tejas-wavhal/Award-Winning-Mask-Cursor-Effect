fd3ce8d1e94f52323949fc2499c437921a203852
Tejas <twavhal7514@gmail.com>
2023-08-13 17:12:36 +0530

    added &apos;

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 'use client'
 import styles from './page.module.scss'
-import { useState } from 'react';  
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 import useMousePosition from './utils/useMousePosition';
 
@@ -12,21 +12,21 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <motion.div 
+      <motion.div
         className={styles.mask}
         animate={{
-          WebkitMaskPosition: `${x - (size/2)}px ${y - (size/2)}px`,
+          WebkitMaskPosition: `${x - (size / 2)}px ${y - (size / 2)}px`,
           WebkitMaskSize: `${size}px`,
         }}
-        transition={{ type: "tween", ease: "backOut", duration:0.5}}
+        transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
       >
-          <p onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}}>
-            A visual designer - with skills that haven't been replaced by A.I (yet) - making good shit only if the paycheck is equally good.
-          </p>
+        <p onMouseEnter={() => { setIsHovered(true) }} onMouseLeave={() => { setIsHovered(false) }}>
+          A visual designer - with skills that haven&apos;t been replaced by A.I (yet) - making good shit only if the paycheck is equally good.
+        </p>
       </motion.div>
 
       <div className={styles.body}>
-        <p>I'm a <span>selectively skilled</span> product designer with strong focus on producing high quality & impactful digital experience.</p>
+        <p>I&apos;m a <span>selectively skilled</span> product designer with strong focus on producing high quality & impactful digital experience.</p>
       </div>
 
     </main>
